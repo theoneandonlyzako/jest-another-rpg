c950392d38b49cc72a1c6759023d5f800e6ee3e0
Zak Stamps <theoneandonlyzako@gmail.com>
2021-05-15 18:07:47 -0700

    added player health

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,7 +2,7 @@ const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
-console.log(new Potion());
+// console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -33,4 +33,33 @@ test('gets inventory from player or returns false', () => {
   player.inventory = [];
 
   expect(player.getInventory()).toEqual(false);
+});
+
+test("gets player's health value", () => {
+  const player = new Player('Dave');
+
+  expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+  const player = new Player('Dave');
+
+  expect(player.isAlive()).toBeTruthy();
+
+  player.health = 0;
+
+  expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+  const player = new Player('Dave');
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+
+  expect(player.health).toBe(0);
 });
